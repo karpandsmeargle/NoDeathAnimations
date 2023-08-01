bc6aec26ce53db764033459fa30d9c00de483591
karpandsmeargle <133045905+karpandsmeargle@users.noreply.github.com>
2023-08-01 15:05:09 -0700

    Fix typo in modId

--- src/main/java/nodeathanimations/nodeathanimations/NDAConfigModel.java
+++ src/main/java/nodeathanimations/nodeathanimations/NDAConfigModel.java
@@ -3,7 +3,7 @@ package nodeathanimations.nodeathanimations;
 import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.Modmenu;
 
-@Modmenu(modId = "nodeaathanimations")
+@Modmenu(modId = "nodeathanimations")
 @Config(name = "ndaconfig", wrapperName = "NDAConfig")
 public class NDAConfigModel {
     public boolean enabled = true;
