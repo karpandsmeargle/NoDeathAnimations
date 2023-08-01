283a903016747306b9a17044d7e58ad9675546d9
karpandsmeargle <133045905+karpandsmeargle@users.noreply.github.com>
2023-08-01 15:04:28 -0700

    Implement ModMenu integration

--- src/main/java/nodeathanimations/nodeathanimations/NDAConfigModel.java
+++ src/main/java/nodeathanimations/nodeathanimations/NDAConfigModel.java
@@ -1,7 +1,9 @@
 package nodeathanimations.nodeathanimations;
 
 import io.wispforest.owo.config.annotation.Config;
+import io.wispforest.owo.config.annotation.Modmenu;
 
+@Modmenu(modId = "nodeaathanimations")
 @Config(name = "ndaconfig", wrapperName = "NDAConfig")
 public class NDAConfigModel {
     public boolean enabled = true;
