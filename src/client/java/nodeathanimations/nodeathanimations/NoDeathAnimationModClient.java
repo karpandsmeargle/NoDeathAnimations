d4491366266379d6a3b8af73b19f0b95b4fcc105
karpandsmeargle <133045905+karpandsmeargle@users.noreply.github.com>
2023-08-01 18:38:34 -0700

    Convert indentation to spaces

--- src/client/java/nodeathanimations/nodeathanimations/NoDeathAnimationModClient.java
+++ src/client/java/nodeathanimations/nodeathanimations/NoDeathAnimationModClient.java
@@ -3,8 +3,8 @@ package nodeathanimations.nodeathanimations;
 import net.fabricmc.api.ClientModInitializer;
 
 public class NoDeathAnimationModClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
 }
