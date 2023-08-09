358cbf050287773a391b78cdb83bab5e905aceb7
karpandsmeargle <133045905+karpandsmeargle@users.noreply.github.com>
2023-08-09 18:01:28 -0400

    Move all to client and update metadata

--- src/client/java/nodeathanimations/nodeathanimations/NoDeathAnimationModClient.java
+++ src/client/java/nodeathanimations/nodeathanimations/NoDeathAnimationModClient.java
@@ -1,10 +1,14 @@
 package nodeathanimations.nodeathanimations;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.fabricmc.api.ClientModInitializer;
 
 public class NoDeathAnimationModClient implements ClientModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("nodeathanimations");
     @Override
     public void onInitializeClient() {
-        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+        LOGGER.info("NoDeathAnimation Mod initialized");
     }
 }
