393f34c8d3e8d88fbe009b49877afee569d33ebb
karpandsmeargle <133045905+karpandsmeargle@users.noreply.github.com>
2024-07-11 01:37:31 -0400

    Upload 1.20.5, 1.20.6, 1.21

--- README.md
+++ README.md
@@ -1,2 +1,105 @@
 # NoDeathAnimations
 Removes entity tilt and fall over death animation. Similar to the Tweakeroo feature in the DISABLE menu. Motivation was to have this feature in a standalone mod that is fair play for multiplayer. Does not disable targeting dead entities, which is considered an unfair advantage.
+
+Version Data, used in automation:
+```py
+# Java 21, Gradle 8.8
+{
+    "minecraft_version": "1.21",
+    "loader_version": "0.15.0",
+    "yarn_mappings": "1.21+build.8",
+    "fabric_version": "0.100.6+1.21",
+},
+{
+    "minecraft_version": "1.20.6",
+    "loader_version": "0.15.0",
+    "yarn_mappings": "1.20.6+build.3",
+    "fabric_version": "0.100.4+1.20.6",
+},
+{
+    "minecraft_version": "1.20.5",
+    "loader_version": "0.15.0",
+    "yarn_mappings": "1.20.5+build.1",
+    "fabric_version": "0.97.8+1.20.5",
+},
+
+# Java 17, Gradle 8.2
+{
+    "minecraft_version": "1.20.4",
+    "loader_version": "0.15.0",
+    "yarn_mappings": "1.20.4+build.3",
+    "fabric_version": "0.91.3+1.20.4",
+},
+{
+    "minecraft_version": "1.20.3",
+    "loader_version": "0.15.0",
+    "yarn_mappings": "1.20.3+build.1",
+    "fabric_version": "0.91.1+1.20.3",
+},
+{
+    "minecraft_version": "1.20.2",
+    "loader_version": "0.15.0",
+    "yarn_mappings": "1.20.2+build.4",
+    "fabric_version": "0.91.2+1.20.2",
+},
+{
+    "minecraft_version": "1.20.1",
+    "loader_version": "0.15.0",
+    "yarn_mappings": "1.20.1+build.10",
+    "fabric_version": "0.91.0+1.20.1",
+},
+{
+    "minecraft_version": "1.20",
+    "loader_version": "0.15.0",
+    "yarn_mappings": "1.20+build.1",
+    "fabric_version": "0.83.0+1.20",
+},
+{
+    "minecraft_version": "1.19.4",
+    "loader_version": "0.15.0",
+    "yarn_mappings": "1.19.4+build.2",
+    "fabric_version": "0.87.2+1.19.4",
+},
+{
+    "minecraft_version": "1.19.3",
+    "loader_version": "0.15.0",
+    "yarn_mappings": "1.19.3+build.5",
+    "fabric_version": "0.76.1+1.19.3",
+},
+{
+    "minecraft_version": "1.19.2",
+    "loader_version": "0.15.0",
+    "yarn_mappings": "1.19.2+build.28",
+    "fabric_version": "0.77.0+1.19.2",
+},
+{
+    "minecraft_version": "1.19.1",
+    "loader_version": "0.15.0",
+    "yarn_mappings": "1.19.1+build.6",
+    "fabric_version": "0.58.5+1.19.1",
+},
+{
+    "minecraft_version": "1.19",
+    "loader_version": "0.15.0",
+    "yarn_mappings": "1.19+build.4",
+    "fabric_version": "0.58.0+1.19",
+},
+{
+    "minecraft_version": "1.18.2",
+    "loader_version": "0.15.0",
+    "yarn_mappings": "1.18.2+build.4",
+    "fabric_version": "0.77.0+1.18.2",
+},
+{
+    "minecraft_version": "1.18.1",
+    "loader_version": "0.15.0",
+    "yarn_mappings": "1.18.1+build.22",
+    "fabric_version": "0.46.6+1.18",
+},
+{
+    "minecraft_version": "1.18",
+    "loader_version": "0.15.0",
+    "yarn_mappings": "1.18+build.1",
+    "fabric_version": "0.46.6+1.18",
+},
+```
--- automation.py
+++ automation.py
@@ -3,82 +3,22 @@ import subprocess
 MOD_VERSION = "1.0.2"
 VERSION_DATA = [
     {
-        "minecraft_version": "1.20.4",
+        "minecraft_version": "1.21",
         "loader_version": "0.15.0",
-        "yarn_mappings": "1.20.4+build.3",
-        "fabric_version": "0.91.3+1.20.4",
+        "yarn_mappings": "1.21+build.8",
+        "fabric_version": "0.100.6+1.21",
     },
     {
-        "minecraft_version": "1.20.3",
+        "minecraft_version": "1.20.6",
         "loader_version": "0.15.0",
-        "yarn_mappings": "1.20.3+build.1",
-        "fabric_version": "0.91.1+1.20.3",
+        "yarn_mappings": "1.20.6+build.3",
+        "fabric_version": "0.100.4+1.20.6",
     },
     {
-        "minecraft_version": "1.20.2",
+        "minecraft_version": "1.20.5",
         "loader_version": "0.15.0",
-        "yarn_mappings": "1.20.2+build.4",
-        "fabric_version": "0.91.2+1.20.2",
-    },
-    {
-        "minecraft_version": "1.20.1",
-        "loader_version": "0.15.0",
-        "yarn_mappings": "1.20.1+build.10",
-        "fabric_version": "0.91.0+1.20.1",
-    },
-    {
-        "minecraft_version": "1.20",
-        "loader_version": "0.15.0",
-        "yarn_mappings": "1.20+build.1",
-        "fabric_version": "0.83.0+1.20",
-    },
-    {
-        "minecraft_version": "1.19.4",
-        "loader_version": "0.15.0",
-        "yarn_mappings": "1.19.4+build.2",
-        "fabric_version": "0.87.2+1.19.4",
-    },
-    {
-        "minecraft_version": "1.19.3",
-        "loader_version": "0.15.0",
-        "yarn_mappings": "1.19.3+build.5",
-        "fabric_version": "0.76.1+1.19.3",
-    },
-    {
-        "minecraft_version": "1.19.2",
-        "loader_version": "0.15.0",
-        "yarn_mappings": "1.19.2+build.28",
-        "fabric_version": "0.77.0+1.19.2",
-    },
-    {
-        "minecraft_version": "1.19.1",
-        "loader_version": "0.15.0",
-        "yarn_mappings": "1.19.1+build.6",
-        "fabric_version": "0.58.5+1.19.1",
-    },
-    {
-        "minecraft_version": "1.19",
-        "loader_version": "0.15.0",
-        "yarn_mappings": "1.19+build.4",
-        "fabric_version": "0.58.0+1.19",
-    },
-    {
-        "minecraft_version": "1.18.2",
-        "loader_version": "0.15.0",
-        "yarn_mappings": "1.18.2+build.4",
-        "fabric_version": "0.77.0+1.18.2",
-    },
-    {
-        "minecraft_version": "1.18.1",
-        "loader_version": "0.15.0",
-        "yarn_mappings": "1.18.1+build.22",
-        "fabric_version": "0.46.6+1.18",
-    },
-    {
-        "minecraft_version": "1.18",
-        "loader_version": "0.15.0",
-        "yarn_mappings": "1.18+build.1",
-        "fabric_version": "0.46.6+1.18",
+        "yarn_mappings": "1.20.5+build.1",
+        "fabric_version": "0.97.8+1.20.5",
     },
 ]
 
@@ -105,7 +45,7 @@ yarn_mappings={yarn_mappings}
 loader_version={loader_version}
 
 # Mod Properties
-mod_version=1.0.2
+mod_version={MOD_VERSION}
 maven_group=nodeathanimations.nodeathanimations
 archives_base_name=nodeathanimations
 
